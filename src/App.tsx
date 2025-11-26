9eaeea6fbaefda31f439571559f1e5ce7e2b3d9c
andreabalillo <andrea.balillo@gmail.com>
2025-11-26 12:26:22 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-slate-900">
+      <h1 className="text-5xl font-bold text-teal-400">
+        THUNDERSTACK
+      </h1>
+    </div>
+  )
+}
